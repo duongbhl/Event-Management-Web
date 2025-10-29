d3c4cd17ab9ecc7420be9864ee5d09a815651663
duongbhl <duongphamhong279@gmail.com>
2025-10-29 17:00:07 +0700

    config middleware utils

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,12 +1,18 @@
 import express from "express"
 import cors from "cors"
 import morgan from "morgan"
+import { errorHandler } from "./middleware/auth.middleware"
+import { authRouter } from "./routes/auth.route"
 
 export const app = express()
 
 app.use(cors())
 app.use(express.json())
 app.use(morgan("dev"))
+
+app.use("api/auth",authRouter)
+
+app.use(errorHandler)
 
 
 
---
+++ backend/src/config/db.ts
@@ -0,0 +1,15 @@
+import Mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+export const connectDB = async () => {
+    try {
+        await Mongoose.connect(
+            process.env.DB_URI,
+        );
+        console.log("MongoDB connected");
+    } catch (error) {
+        console.error("MongoDB connection error:", error);
+        process.exit(1);
+    }
+};
