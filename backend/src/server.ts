05cbd01f64b0ae5aa2cce5eb13b7de51972b0b47
duongbhl <duongphamhong279@gmail.com>
2025-10-29 17:00:07 +0700

    config middleware utils

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,9 +1,11 @@
 import { app } from "./app"
+import { connectDB } from "./config/db"
 
 
 const PORT = 5000
 
 const test = async () =>{
+    await connectDB()
     app.listen(PORT,()=>{
         console.log(`✅SERVER BẮT ĐẦU CHẠY TRÊN CỔNG http://localhost:${PORT}`);
     })
